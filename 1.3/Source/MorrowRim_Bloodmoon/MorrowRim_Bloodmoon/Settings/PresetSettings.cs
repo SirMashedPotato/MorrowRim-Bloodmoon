be7ba94887522dda5c8ed53da339933b0a51e597
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2021-10-06 10:43:27 +1300

    Misc changes
    Hunter's gifts now increase a pawns market value, assimilated lycanthropy buffed, marked for the hunt now lasts twice as long

--- 1.3/Source/MorrowRim_Bloodmoon/MorrowRim_Bloodmoon/Settings/PresetSettings.cs
+++ 1.3/Source/MorrowRim_Bloodmoon/MorrowRim_Bloodmoon/Settings/PresetSettings.cs
@@ -16,17 +16,32 @@ namespace MorrowRim_Bloodmoon
             settings.raidModifier = 1f;
             settings.minTicksPerEvent = 10000;
             settings.maxTicksPerEvent = 20000;
+            settings.extraAvatarChance = 10;
+            settings.minNumberOfLootDrops = 1;
+            settings.maxNumberOfLootDrops = 3;
+            settings.enableBloodmoonForAll = false;
+            settings.enableMessages = true;
             settings.chanceOfGiftedWerewolf = 5;
-            settings.incidentIntervalCycle = 10f;
-            settings.incidentMinimumDaysCycle = 10;
-            settings.incidentIntervalRogue = 15f;
-            settings.incidentMinimumDaysRogue = 15;
+            settings.infectionChance = 50;
+            settings.enableBloodmoonCycleForAll = false;
+            settings.enableLootDrops = true;
+            settings.elixirFailChance = 10;
+            settings.maxWerewolfNum = 100;
+            settings.minWerewolfNum = 1;
+            settings.werewolfStrength = 0;
+            settings.animalMarking = true;
             settings.minTicksPerMark = 15000;
             settings.maxTicksPerMark = 30000;
-            settings.maxWerewolfNum = 100;
-            settings.chanceOfGiftedWerewolf = 5;
-
+            settings.incidentIntervalCycle = 10f;
+            settings.incidentIntervalRogue = 15f;
+            settings.enableWerewolfPack = true;
+            settings.incidentMinimumDaysCycle = 10;
+            settings.incidentMinimumDaysRogue = 15;
+            settings.enableLetters = false;
+            settings.huntersGiftMaximum = 1f;
+            settings.huntersGiftBuildup = 0.1f;
             settings.enableStrengthScaling = false;
+            settings.enableBloodmoonAmbushes = true;
 
             sound.PlayOneShotOnCamera();
         }
