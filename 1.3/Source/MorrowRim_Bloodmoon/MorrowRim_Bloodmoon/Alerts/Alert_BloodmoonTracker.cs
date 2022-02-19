e7bd7f28e513d6c200b0b2fc4ad45a72d0c1b6b7
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-02-20 09:02:40 +1300

    Update to 1.3.10

---
+++ 1.3/Source/MorrowRim_Bloodmoon/MorrowRim_Bloodmoon/Alerts/Alert_BloodmoonTracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace MorrowRim_Bloodmoon
+{
+    class Alert_BloodmoonTracker : Alert
+    {
+        public Alert_BloodmoonTracker()
+        {
+            this.defaultLabel = "Bloodmoon_Alert_TrackerLabel".Translate();
+            this.defaultPriority = AlertPriority.Medium;
+        }
+
+        public override AlertReport GetReport()
+        {
+            return ModSettings_Utility.EnableStrengthScaling() ? AlertReport.Active : AlertReport.Inactive;
+        }
+
+        public override TaggedString GetExplanation()
+        {
+            return "Bloodmoon_Alert_WerewolfTrackerDescription".Translate(ModSettings_Utility.GetBloodStrength() * 100);
+        }
+    }
+}
